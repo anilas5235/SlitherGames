f15a462f73ea0f93e564f960ebdf25e946418366
anilas5235 <niklas-borchers@outlook.de>
2024-01-14 22:14:27 +0100

    data

---
+++ Assets/Scripts/UpgradeData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    [CreateAssetMenu(fileName = "UpgradeData", menuName = "DataObjects/UpgradeData", order = 0)]
+    public class UpgradeData : ScriptableObject
+    {
+        public SpeedOffsetPair[] StateData;
+
+        public SpeedOffsetPair GetStateAtLevel(int level)
+        {
+            if (level < 0 || level >= StateData.Length) return default;
+            return StateData[level];
+        }
+    }
+
+    [Serializable]
+    public struct SpeedOffsetPair
+    {
+        public float Speed;
+        public int Offset;
+    }
+}
